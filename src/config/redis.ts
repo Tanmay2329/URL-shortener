60fd71e6df1c77f5bc12073f53a71e78380de447
tanmay2329 <tanmaychandorkar23@gmail.com>
2026-04-23 18:11:12 +0530

    Final Redis Connected

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -4,7 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL, // must be rediss://
+  url: process.env.REDIS_URL, 
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
 });
 
 redisClient.on("error", (err) => {
